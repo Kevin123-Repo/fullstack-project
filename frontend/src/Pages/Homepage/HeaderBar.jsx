e6fabfb09437804b7868f238f7f32d4f3f21d843
Kevin <josungtse@hotmail.co.uk>
2025-03-13 20:19:40 +0000

    Added Header NavBar

--- frontend/src/Pages/Homepage/HeaderBar.jsx
+++ frontend/src/Pages/Homepage/HeaderBar.jsx
@@ -3,8 +3,50 @@ import {Link}  from "react-router-dom"
 function HeaderBar() {
     return(
         <div className="App">
-            <Link className="navbar-brand" to="/"> My Web App</Link>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            My Web App
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/ecommerce">
+                  E-Commerce
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
+              {user ? (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/dashboard">
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <button className="btn btn-danger nav-link" onClick={handleLogout}>
+                      Logout
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
+      </nav>
+    </div>
     )
 }
 
